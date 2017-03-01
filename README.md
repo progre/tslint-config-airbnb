45527f691b3003d6824085c4b9bf410ad34cc58c
progre <djyayutto@gmail.com>
2017-03-01 23:25:09 +0900

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# TSLint Config Airbnb
+
+[![NPM version](https://img.shields.io/npm/v/tslint-config-airbnb.svg?style=flat)](https://www.npmjs.com/package/tslint-config-airbnb)
+
+> A [TSLint config](https://palantir.github.io/tslint/usage/tslint-json/) for [Airbnb JavaScript Style Guide](https://github.com/airbnb/javascript)
+
+## Installation
+
+```sh
+npm install tslint-config-airbnb --save
+```
+
+## Usage
+
+In `tslint.json`:
+
+```json
+{
+  "extends": "tslint-config-airbnb"
+}
+```
+
+### Rules
+
+* [TSLint](https://www.npmjs.com/package/tslint)
+* [TSLint ESLint Rules](https://www.npmjs.com/package/tslint-eslint-rules)
+* [tslint-microsoft-contrib](https://www.npmjs.com/package/tslint-microsoft-contrib)
+
+## License
+
+Apache 2.0
---
+++ tslint.js
@@ -0,0 +1,126 @@
+const path = require('path');
+
+module.exports = {
+  rulesDirectory: [
+    path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules'),
+    path.join(path.dirname(require.resolve('tslint-microsoft-contrib')), './')
+  ],
+  rules: {
+    'prefer-const': true, // 2.1, 13.1
+    'no-var-keyword': true, // 2.2
+    'object-literal-shorthand': true, // 3.3, 3.4
+    'object-literal-key-quotes': [
+      true,
+      'as-needed'
+    ], // 3.6
+    'prefer-array-literal': true, // 4.1
+    quotemark: [
+      true,
+      'single',
+      'jsx-double'
+    ], // 6.1
+    'no-eval': true, // 6.4
+    'space-before-function-paren': [
+      true,
+      {
+        anonymous: 'always', // 7.11
+        named: 'never' // 18.3
+      }
+    ],
+    align: [
+      true,
+      'arguments',
+      'parameters'
+    ], // 7.15
+    'ter-prefer-arrow-callback': [true], // 8.1
+    'arrow-parens': false,
+    'ter-arrow-parens': [
+      true,
+      'as-needed',
+      {
+        'requireForBlockBody': true
+      }
+    ], // 8.4
+    'one-variable-per-declaration': [
+      true,
+      'ignore-for-loop'
+    ], // 13.2
+    'no-increment-decrement': true, // 13.6
+    'triple-equals': [
+      true,
+      'allow-null-check'
+    ], // 15.1
+    // 'no-boolean-literal-compare': true, // 15.3  disabled by https://github.com/Microsoft/vscode-tslint/issues/70
+    // 'strict-boolean-expressions': true, // 15.3  disabled by https://github.com/Microsoft/vscode-tslint/issues/70
+    'brace-style': [
+      true,
+      '1tbs',
+      {
+        "allowSingleLine": true
+      }
+    ], // 16.2
+    'comment-format': [
+      true,
+      'check-space'
+    ], // 17.3
+    indent: [
+      true,
+      'space'
+    ],
+    'ter-indent': [
+      true,
+      2,
+      { 'SwitchCase': 1 }
+    ], // 18.1
+    whitespace: [
+      true,
+      'check-branch', // 18.3
+      'check-decl', // 18.4
+      'check-operator', // 18.4
+      'check-preblock', // 18.2
+      'check-separator' // 18.2
+    ],
+    eofline: true, // 18.5
+    'space-in-parens': [
+      true,
+      'never'
+    ], // 18.9
+    'array-bracket-spacing': [
+      true,
+      'never'
+    ], // 18.10
+    'object-curly-spacing': [
+      true,
+      'always'
+    ], // 18.11
+    'max-line-length': [
+      true,
+      100
+    ], // 18.12
+    'trailing-comma': [
+      true,
+      {
+        multiline: 'always',
+        singleline: 'never'
+      }
+    ], // 19.2
+    semicolon: [
+      true,
+      'always'
+    ], // 20.1
+    radix: true, // 21.3
+    'no-construct': true, // 21.3, 21.6
+    'function-name': [
+      true,
+      {
+        'static-method-regex': '^[a-z][\\w\\d]+$'
+      } // 22.1
+    ],
+    'variable-name': [
+      true,
+      'check-format' // 22.2
+    ],
+    'no-var-self': true, // 22.5
+    'import-name': true // 22.6
+  }
+};
