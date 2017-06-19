b4210920617bcf1366e5cf11ba473a15bfb47fac
progre <djyayutto@gmail.com>
2017-06-19 19:35:46 +0900

    feat: disallow null check

--- tslint.js
+++ tslint.js
@@ -53,8 +53,7 @@ module.exports = {
     ], // 13.2
     'no-increment-decrement': true, // 13.6
     'triple-equals': [
-      true,
-      'allow-null-check'
+      true
     ], // 15.1
     // 'no-boolean-literal-compare': true, // 15.3  disabled by https://github.com/Microsoft/vscode-tslint/issues/70
     // 'strict-boolean-expressions': true, // 15.3  disabled by https://github.com/Microsoft/vscode-tslint/issues/70
