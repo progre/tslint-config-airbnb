1990067ff0ea86939d5ed2af2a0a239b90dba355
progre <djyayutto@gmail.com>
2017-09-14 18:57:06 +0900

    chore: update sample

--- sample/index.ts
+++ sample/index.ts
@@ -4,6 +4,21 @@ class SampleClass {
   method ( ) {
     this.member = {};
     const shorthand = 1;
+    const isValid: boolean = null;
+
+    // bad
+    if (isValid === true) {
+      // ...
+    }
+
+    // good
+    if (isValid) {
+      // ...
+    }
+
+    new Promise((resolve, reject) => {
+    });
+
     return {
       a: 1,
       shorthand,
